213cac5b8738292451c3672b0da1d5a832ac6025
vinthen <nehtniv@gmail.com>
2017-08-07 15:08:25 +0800

    update design / content

--- whitecollar/js/layout.js
+++ whitecollar/js/layout.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
 
+let $body = $("body");
 let $card = $(".cardBoard");
 let $cardTotal = $(".pager--total");
+
+//fix ios flexbox issue
+{
+    $(".cardwpr").each(function(){
+        $(this)
+        .wrap("<div class='wrapTable'></div>");
+    });
+
+}
 
 //偶數卡片設定較深背景
 $card.filter(":odd").addClass("dark");
 
 //標示頁數
 {
-    let sum = $card.length -1;
+    let sum = $card.length -1; //扣掉開頭畫面
     $cardTotal.text(sum);
 
     $card.each(function(){
@@ -19,9 +29,9 @@ $card.filter(":odd").addClass("dark");
 }
 
 //only for firefox preview
-// $card.filter(":lt(22)").remove();
+// $card.filter(":lt(1)").remove();
 
-
+/*
 //init ScrollMagic
 var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
@@ -39,8 +49,76 @@ for(var i=0; i<slides.length; i++){
     }).setPin(slides[i])   
     .addTo(controller);
 }
+*/
 
-//debug
+
+//設定卡片圖檔容器高度
+function setImageHeight(){
+    let wheight = $(window).height();
+    $card.each(function(){
+
+        if($(this).find(".desc").length){
+
+            let descHeight = $(this).find(".desc").outerHeight();
+
+            if($(this).find(".cardwpr").length){
+
+                let imgHeight =  wheight - descHeight; 
+
+                $(this)
+                .find(".wrapTable").height(imgHeight + "px")
+                .find(".cardwpr").css({
+                    height: imgHeight + "px",
+                    minHeight:  imgHeight + "px"                    
+                });
+
+            }
+
+        }
+    });
+
+}
+
+
+//interview RWD, clone content
+{
+
+    $(".interview").each(function(){
+
+        if($(this).find(".interview--brief").length){
+            var cloneBrief = $(this).find(".interview--brief").clone().addClass("cloneBrief");
+            $(this).find(".interview--content").prepend(cloneBrief);
+            // console.log("interview done");
+        }
+
+    });    
+
+
+}
+
+//show viewport size
+//remove before publish
+function sizeDetect(){
+    var wwidth = $(window).width();
+    var wheight = $(window).height();
+    $("#wwidth").text("vw:"+wwidth+"px"); //remove before publish
+    $("#wheight").text("vh:"+wheight+"px"); //remove before publish
+
+    if(wheight > wwidth){
+        $body.addClass("compactMode");
+    } else {
+        $body.removeClass("compactMode");
+    }
+
+}
+
+$(window).resize(function(){
+    sizeDetect();
+    setImageHeight();
+}).trigger("resize");
+
+
+//remove before publish
 console.log("layout.js done");
 
 });
