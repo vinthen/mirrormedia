77eec1939384a1cd68f2d094cad06089ea3dd8e8
vinthen <nehtniv@gmail.com>
2017-09-25 10:34:10 +0800

    設計更新

--- aboriginalname/js/layout.js
+++ aboriginalname/js/layout.js
@@ -1,41 +1,60 @@
 $(document).ready(function(){
 
+//文章註解    
+let $annoBtn = $(".annotation--mark");
 
+$annoBtn.click(function(){
+    $(this).parent().next(".annotation").toggleClass("show");
+});
 
+//延伸閱讀 tab
+{
+$("ul.tabs li").click(function(){
 
+    var tab_id = $(this).attr("data-tab");
 
-/*
-var $window = $(window);
+    $("ul.tabs li").removeClass("current");
+    $(".tabcontent").removeClass("current");
 
-function scrollDetect(){
-    var scroll_top = $window.scrollTop();
-    
-    //scrollTop detection
-    $("#sTop").text("scrollTop:"+scroll_top+"px");
-    
+    $(this).addClass("current");
+    $("#" + tab_id ).addClass("current");
+
+});
 }
 
-function sizeDetect(){
-    
-    //viewport
-    var wwidth = $window.width();
-    var wheight = $window.height();
-    $("#wwidth").text("vw:"+wwidth+"px");
-    $("#wheight").text("vh:"+wheight+"px");
-    
-    //content zoom
-    var zwidth = $("#zw").width();
-    var zheight = $("#zh").height();
-    $("#zwidth").text("zw:"+zwidth+"px");
-    $("#zheight").text("zh:"+zheight+"px");    
+//lightbox
+$(".lbtrigger").click(function(){
+    lbOpen($(this));
+});   
+
+$(".lbClose").click(function(){
+    lbClose();
+});
+
+$(".lbOverlay").click(function(){
+    lbClose();
+});
+
+
+function lbOpen(target){
+    var lb_id = target.attr("data-lb");
+    $("#" + lb_id ).addClass("active");
+    $("html").addClass("stop-scroll");
+}
+
+function lbClose(){
+    $(".lightbox").removeClass("active");
+    $("html").removeClass("stop-scroll");
 }
 
 
-// window scroll
-$window.scroll(scrollDetect);
 
-//window resize
-$window.resize(sizeDetect).trigger("resize");
-*/
+//測試用選單，發佈前請移除
+$("body").append("<div id='testnav'></div>");
+$("#testnav").load("test.html",function(){
+    console.log("test nav loaded");
+});
+
+
 
 });
